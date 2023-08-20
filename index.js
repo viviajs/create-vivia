a8634787afa92c6cfc95d7b78a25cd4b79367a1a
Saurlax <saurlax@qq.com>
2023-08-20 20:56:22 +0800

    fix: make everything work

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const chalk = require('chalk')
 const fs = require('fs')
 const path = require('path')
 
-fs.cpSync(path.join(__dirname, 'scaffold'), '.')
+fs.cpSync(path.join(__dirname, 'scaffold'), '.', { recursive: true })
 console.info(chalk.green('Done'))
